90394e1bf1a70393347eb7b69a079d3c41dbb32b
JulianMarsal <51145868+JulianMarsal@users.noreply.github.com>
2020-05-14 04:42:50 -0300

    Update urls.py

--- bookflix/urls.py
+++ bookflix/urls.py
@@ -3,8 +3,13 @@ from .views import *
 
 urlpatterns = [
     path('', welcome, name='welcome'),
-    path('login', login_propio, name='login'),
+    path('login/', login_propio, name='login/'),
     path('logout/', logout, name='logout'),#no se para que son los names pero por las duda los dejo
     path('register_page/', register_page, name='registrar'),
-]
+    path('barra/', barra, name='barra'),
+    path('base/', base, name='base'),
+    path('perfil/', perfil, name='perfil'),
+    path('publicaciones/', publicaciones, name='publicaciones'),
+    path('publicacion/', publicacion, name='publicacione'),
 
+]
